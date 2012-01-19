ff183d2f6f000c5e118a577534438b009e571f85
Tim Krones <t.krones@gmx.net>
2012-01-19 16:30:30 +0100

    Clean up + refactor

--- local_apps/wt_articles/management/commands/update_wiki_articles.py
+++ local_apps/wt_articles/management/commands/update_wiki_articles.py
@@ -1,8 +1,9 @@
-from django.core.management.base import NoArgsCommand, CommandError
 from datetime import datetime
-from wikipydia import query_text_rendered, query_text_raw
-
-from wt_articles.models import ArticleOfInterest, SourceArticle, TranslationRequest
+from django.core.management.base import NoArgsCommand
+from wikipydia import query_text_rendered
+from wt_articles.models import ArticleOfInterest
+from wt_articles.models import SourceArticle
+from wt_articles.models import TranslationRequest
 from wt_articles import DEFAULT_TRANNY
 
 class Command(NoArgsCommand):
@@ -13,32 +14,25 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         articles_of_interest = ArticleOfInterest.objects.all()
         for article in articles_of_interest:
-            #article_dict = query_text_raw(article.title,
-                                               #language=article.title_language)
             article_dict = query_text_rendered(article.title,
                                                language=article.title_language.code)
             # don't import articles we already have
-            if SourceArticle.objects.filter(doc_id__exact='%s' % article_dict['revid'],
+            # if SourceArticle.objects.filter(doc_id__exact='%s' % article_dict['revid'],
+            #                                 language=article.title_language):
+            if SourceArticle.objects.filter(title__exact='%s' % article.title,
                                             language=article.title_language):
                 continue
             try:
                 source_article = SourceArticle(title=article.title,
                                                language=article.title_language,
-                                               # source_text = article_dict['text'],
                                                source_text=article_dict['html'],
                                                timestamp=datetime.now(),
                                                doc_id=article_dict['revid'])
                 source_article.save()
-                #tr = TranslationRequest(article=source_article,
-                #                         target_language=article.target_language,
-                #                         date=datetime.now(),
-                #                         translator=DEFAULT_TRANNY)
-                #tr.save()
             except Exception as e:
-                print type(e)
-                print e.args
+                print "Looks like we have an exception of type %s" % type(e)
+                print "Exception args:", e.args
                 try:
                     source_article.delete()
-                    tr.delete()
                 except:
                     pass
