e7e5e7c1e079a826fd2e065a6b60e5d8d9af7c2b
Tim Krones <t.krones@gmx.net>
2012-01-25 15:40:25 +0100

    Fix formatting of imports

--- local_apps/wt_articles/models.py
+++ local_apps/wt_articles/models.py
@@ -1,31 +1,28 @@
+import logging
+import re
+
+import polib
+
+from aifc import Error
+from datetime import datetime
+from urllib import quote_plus, unquote_plus
+
+from BeautifulSoup import BeautifulSoup
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import iri_to_uri
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.encoding import iri_to_uri
+from django.utils.translation import ugettext_lazy as _
 
-#from wt_languages.models import LANGUAGE_CHOICES
-#from wt_languages.models import TARGET_LANGUAGE,SOURCE_LANGUAGE,BOTH
-#from wt_languages.models import LanguageCompetancy
-from wt_articles import TRANSLATORS, TRANSLATION_STATUSES
+from pootle_language.models import Language
+from pootle_project.models import Project
+from pootle_translationproject.models import TranslationProject
+
+from wt_articles import TRANSLATION_STATUSES, TRANSLATORS
 from wt_articles.splitting import determine_splitter
 from wt_translation.models import MachineTranslator
 
-# Pootle-related imports
-from pootle_project.models import Project
-from pootle_language.models import Language
-from pootle_translationproject.models import TranslationProject
-# from pootle_store.filetypes import filetype_choices, factory_classes, is_monolingual
 
-from BeautifulSoup import BeautifulSoup
-from datetime import datetime
-import polib
-import logging
-
-from urllib import quote_plus, unquote_plus
-
-import re
-from aifc import Error
 
 if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
