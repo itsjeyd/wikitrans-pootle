a86bd0607054afc7c09c34437bd640bc434468b5
Tim Krones <t.krones@gmx.net>
2012-03-18 19:46:34 +0100

    Remove more dependencies: Some of them are completely obsolete, others don't need to be listed explicitly in requirements.txt because other packages in dependencies/install.sh depend on them, so apt-get installs them automatically

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -14,7 +14,6 @@ sudo pip install django
 # Install dependencies from requirements.txt
 sudo apt-get install libyaml-0-2
 sudo pip install -I pyyaml
-sudo pip install http://dist.repoze.org/PIL-1.1.6.tar.gz
 
 sudo apt-get install libxml2-dev
 sudo apt-get install libxslt-dev
