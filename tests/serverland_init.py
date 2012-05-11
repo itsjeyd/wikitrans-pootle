f5980d7773f10346d7f68719a7eda16330cbe5e8
Tim Krones <t.krones@gmx.net>
2012-05-11 13:41:10 +0200

    Update with information about remote ServerLand host

--- tests/serverland_init.py
+++ tests/serverland_init.py
@@ -1,9 +1,8 @@
 from wt_translation.models import MachineTranslator, ServerlandHost
 
 host = ServerlandHost()
-host.shortname = "local"
-host.url = "http://0.0.0.0:6666"
-host.token = "7594f0db"
-host.description = "A local XML-RPC serverland host"
-
+host.shortname = "remote"
+host.url = "http://www.dfki.de/mt-serverland/dashboard/api/xml/"
+host.token = "4c4ab8e2"
+host.description = "ServerLand host running on DFKI servers"
 host.save()
