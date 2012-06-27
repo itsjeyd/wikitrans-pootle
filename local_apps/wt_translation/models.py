c655a37af9425f07e7ec8981aeb7d1f004282261
Tim Krones <t.krones@gmx.net>
2012-06-27 08:47:52 +0200

    Remove any markup that LucyWorker inserts into the translation result before trying to align source sentences with it.

--- local_apps/wt_translation/models.py
+++ local_apps/wt_translation/models.py
@@ -272,6 +272,7 @@ class ServerlandHost(models.Model):
                     et = utils.element_tree(response)
                     result = et.findtext('result')
                     result = re.sub('### (\[\[YAHOO_SPLITTER\]\]\n)?(### )?', '', result)
+                    result = re.sub('(<[A-Z]\[)?(\]>)?', '', result)
                     result_sentences = [
                         sentence.strip() for sentence in
                         utils.clean_string(result.strip()).split('\n')
