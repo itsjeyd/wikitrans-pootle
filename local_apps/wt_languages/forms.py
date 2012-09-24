d89fdd6e16c120f6d24ba907274bb9f22d27fcd3
Tim Krones <t.krones@gmx.net>
2012-09-24 17:19:12 +0200

    Remove trailing whitespace

--- local_apps/wt_languages/forms.py
+++ local_apps/wt_languages/forms.py
@@ -6,11 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 from wt_languages.models import LanguageCompetancy
 
 class LanguageCompetancyForm(forms.ModelForm):
-    
+
     class Meta:
         model = LanguageCompetancy
         exclude = ('user','updated')
-    
+
     def __init__(self, user=None, *args, **kwargs):
         self.user = user
         super(LanguageCompetancyForm, self).__init__(*args, **kwargs)
