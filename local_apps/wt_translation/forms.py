58f57b11155c55a912c771699870b5cea77e73d6
Tim Krones <t.krones@gmx.net>
2012-10-01 17:20:47 +0200

    Remove trailing whitespace

--- local_apps/wt_translation/forms.py
+++ local_apps/wt_translation/forms.py
@@ -12,29 +12,29 @@ from pootle_language.models import Language
 #    def __init__(self, source_language, target_language, *args, **kwargs):
 #        self.source_language = source_language
 #        self.target_language = target_language
-#        
+#
 #        # Call the parent constructor
 #        super(MachineTranslatorSelectorForm, self).__init__(*args, **kwargs)
 #        self.fields['translators'].queryset = MachineTranslator.objects.filter(
 #                            supported_languages__source_language = source_language,
-#                            supported_languages__target_language = target_language                                                   
+#                            supported_languages__target_language = target_language
 #                        )
-#        
+#
 #    translators = forms.ModelMultipleChoiceField(_("Translators"))
-    
+
 class TranslationRequestForm(forms.ModelForm):
     def __init__(self, translation_project=None, *args, **kwargs):
         super(TranslationRequestForm, self).__init__(*args, **kwargs)
-        
+
         if translation_project != None:
             self.translation_project = translation_project
-            
-            self.fields['translator'].queryset = MachineTranslator.objects.filter(supported_languages__in = 
+
+            self.fields['translator'].queryset = MachineTranslator.objects.filter(supported_languages__in =
                             LanguagePair.objects.filter(
-                                Q(source_language=translation_project.project.source_language), 
+                                Q(source_language=translation_project.project.source_language),
                                 Q(target_language=translation_project.language)
                             ))
-        
+
     class Meta:
         model = TranslationRequest
         exclude = ('status', 'external_id', 'timestamp',)
