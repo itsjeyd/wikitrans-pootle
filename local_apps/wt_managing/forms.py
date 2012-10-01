c430fabcc1ec984890fe497efc5b2db642d2801a
Tim Krones <t.krones@gmx.net>
2012-10-01 14:25:13 +0200

    Remove trailing whitespace

--- local_apps/wt_managing/forms.py
+++ local_apps/wt_managing/forms.py
@@ -7,11 +7,11 @@ from wt_managing.models import SentenceReview
 class SentenceReviewForm(forms.ModelForm):
     segment_id = forms.CharField(widget=forms.HiddenInput)
     articlereview = forms.CharField(widget=forms.HiddenInput)
-    
+
     class Meta:
         model = SentenceReview
         fields = ('articlereview', 'segment_id', 'accepted',)
-    
+
     def __init__(self, *args, **kwargs):
         super(SentenceReviewForm, self).__init__(*args, **kwargs)
 
