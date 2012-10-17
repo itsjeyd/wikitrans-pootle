011e81dd6b61a36423377d208512248a00b819e2
Tim Krones <t.krones@gmx.net>
2012-10-17 11:31:15 +0200

    Remove commented-out code

--- local_apps/wt_translation/forms.py
+++ local_apps/wt_translation/forms.py
@@ -4,23 +4,6 @@ from django.db.models import Q
 from wt_translation.models import MachineTranslator, LanguagePair, \
      TranslationRequest
 
-#class MachineTranslatorSelectorForm(forms.Form):
-#   """
-#   A custom form used to select a machine translator to translate a
-#   language pair.
-#   """
-#   def __init__(self, source_language, target_language, *args, **kwargs):
-#       self.source_language = source_language
-#       self.target_language = target_language
-
-#       # Call the parent constructor
-#       super(MachineTranslatorSelectorForm, self).__init__(*args, **kwargs)
-#       self.fields['translators'].queryset = MachineTranslator.objects.filter(
-#           supported_languages__source_language = source_language,
-#           supported_languages__target_language = target_language
-#           )
-
-#   translators = forms.ModelMultipleChoiceField(_("Translators"))
 
 class TranslationRequestForm(forms.ModelForm):
     def __init__(self, translation_project=None, *args, **kwargs):
