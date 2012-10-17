7580fbb8f24b4e4ef5dd2512afbb059dca22cbc6
Tim Krones <t.krones@gmx.net>
2012-10-17 11:24:37 +0200

    Fix formatting: Keep lines to 78 chars or less

--- local_apps/wt_languages/urls.py
+++ local_apps/wt_languages/urls.py
@@ -5,15 +5,20 @@ from wt_languages import views
 
 urlpatterns = patterns('',
     # your language competancies
-    url(r'^$', 'wt_languages.views.language_competancy_list', name="language_competancy_list"),
+    url(r'^$', 'wt_languages.views.language_competancy_list',
+        name="language_competancy_list"),
 
     # new competancy
-    url(r'^new/$', 'wt_languages.views.language_competancy_new', name='language_competancy_new'),
+    url(r'^new/$', 'wt_languages.views.language_competancy_new',
+        name='language_competancy_new'),
 
     # edit competancy
-    url(r'^edit/(\d+)/$', 'wt_languages.views.language_competancy_edit', name='language_competancy_edit'),
+    url(r'^edit/(\d+)/$', 'wt_languages.views.language_competancy_edit',
+        name='language_competancy_edit'),
 
     #destory competancy
-    url(r'^destroy/(\d+)/$', 'wt_languages.views.language_competancy_destroy', name='language_competancy_destroy'),
+    url(r'^destroy/(\d+)/$',
+        'wt_languages.views.language_competancy_destroy',
+        name='language_competancy_destroy'),
 
 )
--- local_apps/wt_languages/views.py
+++ local_apps/wt_languages/views.py
@@ -16,8 +16,10 @@ else:
     notification = None
 
 @login_required
-def language_competancy_list(request, template_name="wt_languages/language_competancy_list.html"):
-    competencies = LanguageCompetancy.objects.filter(user__username=request.user.username)
+def language_competancy_list(
+    request, template_name="wt_languages/language_competancy_list.html"):
+    competencies = LanguageCompetancy.objects.filter(
+        user__username=request.user.username)
 
     return render_to_response(template_name, {
         "competancies": competencies,
@@ -28,12 +30,14 @@ def language_competancy_destroy(request, lc_id):
     language_competancy = LanguageCompetancy.objects.get(pk=lc_id)
     user = request.user
     if language_competancy.user != user:
-        user.message_set.create(message="You can't delete competancies that aren't yours")
+        user.message_set.create(
+            message="You can't delete competancies that aren't yours")
         return HttpResponseRedirect(reverse("language_competancy_list"))
 
     if request.method == "POST" and request.POST["action"] == "delete":
         language_competancy.delete()
-        user.message_set.create(message=_("Successfully deleted competancy '%s'") % language_competancy)
+        user.message_set.create(message=_(
+            "Successfully deleted competancy '%s'") % language_competancy)
         return HttpResponseRedirect(reverse("language_competancy_list"))
     else:
         return HttpResponseRedirect(reverse("language_competancy_list"))
@@ -41,18 +45,24 @@ def language_competancy_destroy(request, lc_id):
     return render_to_response(context_instance=RequestContext(request))
 
 @login_required
-def language_competancy_new(request, form_class=LanguageCompetancyForm, template_name="wt_languages/language_competancy_new.html"):
+def language_competancy_new(
+    request, form_class=LanguageCompetancyForm,
+    template_name="wt_languages/language_competancy_new.html"):
     if request.method == "POST":
         if request.POST["action"] == "create":
             language_competancy_form = form_class(request.user, request.POST)
             if language_competancy_form.is_valid():
-                language_competancy = language_competancy_form.save(commit=False)
+                language_competancy = language_competancy_form.save(
+                    commit=False)
                 language_competancy.user = request.user
                 language_competancy.updated = datetime.now()
                 language_competancy.save()
                 # @@@ should message be different if published?
-                request.user.message_set.create(message=_("Successfully saved competancy '%s'") % language_competancy)
-                return HttpResponseRedirect(reverse("language_competancy_list"))
+                request.user.message_set.create(message=_(
+                    "Successfully saved competancy '%s'") %
+                                                language_competancy)
+                return HttpResponseRedirect(reverse(
+                    "language_competancy_list"))
         else:
             language_competancy_form = form_class()
     else:
@@ -63,20 +73,28 @@ def language_competancy_new(request, form_class=LanguageCompetancyForm, template
     }, context_instance=RequestContext(request))
 
 @login_required
-def language_competancy_edit(request, lc_id, form_class=LanguageCompetancyForm, template_name="wt_languages/language_competancy_edit.html"):
+def language_competancy_edit(
+    request, lc_id, form_class=LanguageCompetancyForm,
+    template_name="wt_languages/language_competancy_edit.html"):
     competancy = get_object_or_404(LanguageCompetancy, id=lc_id)
 
     if request.method == "POST":
         if competancy.user != request.user:
-            request.user.message_set.create(message="You can't edit competancies that aren't yours")
+            request.user.message_set.create(
+                message="You can't edit competancies that aren't yours")
             return HttpResponseRedirect(reverse("language_competancy_list"))
         if request.POST["action"] == "update":
-            language_competancy_form = form_class(request.user, request.POST, instance=competancy)
+            language_competancy_form = form_class(
+                request.user, request.POST, instance=competancy)
             if language_competancy_form.is_valid():
-                language_competancy = language_competancy_form.save(commit=False)
+                language_competancy = language_competancy_form.save(
+                    commit=False)
                 language_competancy.save()
-                request.user.message_set.create(message=_("Successfully updated copmetancy '%s'") % language_competancy)
-                return HttpResponseRedirect(reverse("language_competancy_list"))
+                request.user.message_set.create(message=_(
+                    "Successfully updated copmetancy '%s'") %
+                                                language_competancy)
+                return HttpResponseRedirect(reverse(
+                    "language_competancy_list"))
         else:
             language_competancy_form = form_class(instance=competancy)
     else:
