2c8fce8ecb799349f2bb8fb845439ec9c0c536a0
Tim Krones <t.krones@gmx.net>
2012-10-22 11:54:53 +0200

    Fix formatting: Keep lines to 78 chars or less

--- local_apps/pootle_app/views/base.py
+++ local_apps/pootle_app/views/base.py
@@ -4,14 +4,15 @@
 #
 # This file is part of translate.
 #
-# translate is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
+# translate is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
 #
 # translate is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 #
 # You should have received a copy of the GNU General Public License along with
 # translate; if not, write to the Free Software Foundation, Inc., 59 Temple
